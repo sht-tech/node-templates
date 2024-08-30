bfee506deedc8bd787c4e3e8267d74bbca2efc04
Sriharsha043 <sriharsha0043@gmail.com>
2024-08-30 20:20:52 +0530

    signin

--- v20/middleware/token.js
+++ v20/middleware/token.js
@@ -2,11 +2,13 @@ const jwt = require("jsonwebtoken");
 
 
 exports.generateToken = async (user) => {
-    const token = jwt.sign({ id: user.id },
+    const token = await jwt.sign({ id: user.id },
         "bezkoder-secret-key",
         {
           algorithm: 'HS256',
           allowInsecureKeySizes: true,
           expiresIn: 86400, // 24 hours
         });
+        console.log(token)
+        return {token: token, createdAt: new Date()}
 }
--- v20/routes/user.js
+++ v20/routes/user.js
@@ -1,7 +1,7 @@
 const db = require('../models')
 const User = db.user;
-
-
+const token = require('../middleware/token')
+const bcrypt = require("bcryptjs");
 module.exports = function(app) {
     app.use((req, res, next) => {
         res.header(
@@ -12,8 +12,52 @@ module.exports = function(app) {
       });
       app.get('/test',async (req,res) =>{
         console.log('working')
-       let data = await  User.find()
-       console.log(data)
-        res.send("200")
+       User.find().then((data) => {
+        console.log(data)
+        res.status(200).send(data)
+       }).catch(err => console.log(err))
+      })
+
+      app.post('/api/signin', (req,res)=> {
+        User.findOne({username: req.body.username}).then( async data => {
+            if(data) {
+                console.log(data)
+                var passwordIsValid = bcrypt.compareSync(
+                    req.body.password,
+                    data.password
+                  );
+                  if(passwordIsValid) {
+                    const tokenn = await token.generateToken(data)
+                    console.log(tokenn)
+                    data.tokens = tokenn
+                    data.save().then(() => res.send(tokenn.token)).catch(err => {console.log(err); res.send(err)})
+                    
+                  }
+                  else {
+                    res.status(300).send("incorrect password")
+                  }
+            }
+            else {
+        res.status(300).send("User not found")
+            }
+        })
+      })
+
+      app.post('/api/signup', (req,res)=> {
+        User.findOne({username: req.body.username}).then( async data => {
+            console.log(data)
+            if(data) {
+               res.status(300).send({message:'User already exists'})
+            }
+            else {
+                const user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: bcrypt.hashSync(req.body.password, 8),
+                  })
+        user.save().then(() => res.send({message: 'user registered sucessfully'}).catch(err => res.status(400).send(err)))
+            }
+        })
       })
 }
+// generateToken
