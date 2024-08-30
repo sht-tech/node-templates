695b00cc83c15f6ea14d2b15e1d140634687d847
Shamir7258 <143414391+Shamir7258@users.noreply.github.com>
2024-08-30 23:39:34 +0530

    Update server.js

--- v20/server.js
+++ v20/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 require('./routes/user.js')(app)
 const db = require("./models");
-db.mongoose.connect('mongodb+srv://trinetech3:qGyTJqbvJzhCDg5f@trine.puklq.mongodb.net/?ssl=true&replicaSet=atlas-10nh35-shard-0&authSource=admin&retryWrites=true&w=majority&appName=trine,').then(() => {
+db.mongoose.connect('mongodb+srv://trinetech3:qGyTJqbvJzhCDg5f@trine.puklq.mongodb.net/trine?ssl=true&replicaSet=atlas-10nh35-shard-0&authSource=admin&retryWrites=true&w=majority&appName=trine,').then(() => {
     console.log("Successfully connect to MongoDB.");
     // initial();
   })
